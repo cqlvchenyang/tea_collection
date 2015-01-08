9b54b4a08f908e18fe39121d5493ef7984b7db56
cqlvchenyang <757104454@qq.com>
2015-01-08 16:03:18 +0800

    lvcy

---
+++ includes/config.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * 2014年11月05日
+ * @author lvcy
+ * @version 1.0.0
+ * 数据库配置文件
+ */
+$db_host   = "localhost:3306";
+
+
+$db_name   = "heavendew";
+
+
+$db_user   = "root";
+
+
+$db_pass   = "admin";
+define('CHARSET','utf-8');
+
+?>
---
+++ includes/init.php
@@ -0,0 +1,19 @@
+﻿<?php
+    if(!defined('DEW')){
+        die("帅哥，求不黑！");
+    }
+    define(ROOT_PATH, str_replace('/includes/init.php', '', str_replace('\\', '/', __FILE__)));
+    include ROOT_PATH.'/includes/config.php';
+    include ROOT_PATH.'/includes/app_mysql.php';
+    /*加载smarty所需文件*/
+    $db = new app_mysql($db_host, $db_user, $db_pass, $db_name);
+    $db_host=$db_user=$db_pass=$db_name=NULL;
+    /*初始化smarty对象*/
+    include ROOT_PATH.'/includes/Smarty.class.php';
+    $smarty=new Smarty();
+    $smarty->template_dir="templates";
+    $smarty->compile_dir="compile";
+    $smarty->config_dir="config";
+    $smarty->cache_dir="caches";
+    session_start();
+?>
